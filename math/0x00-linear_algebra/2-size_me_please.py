12b03194575be7e31d8906b5ebd1a6a0d8254ca6
Coderlancce <coderlancce@gmail.com>
2022-12-01 12:39:01 -0500

    [OK] added coments

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+"""That calculates the shape of a matrix
+"""
+
+
 def matrix_shape(matrix):
-    """
-	Recursive function to count in a list the size
+    """Recursive function to count in a list the size
     """
     if type(matrix[0]) is not list:
         return [len(matrix)]
