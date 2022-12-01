713a1feebc6c45185d64a4bca96ae61c699d4aca
Coderlancce <coderlancce@gmail.com>
2022-12-01 11:18:50 -0500

    [OK] task 2

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+That calculates the shape of a matrix
+"""
+
+
+def matrix_shape(matrix):
+    if type(matrix[0]) is not list:
+        return [len(matrix)]
+    else:
+        return [len(matrix)] + matrix_shape(matrix[0])
